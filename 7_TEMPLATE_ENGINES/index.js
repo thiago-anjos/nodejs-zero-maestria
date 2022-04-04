af4811dc7e91aab4a54ab95bca2b74172e8e9b92
thiago-anjos <your_email@example.com>
2022-04-04 05:23:11 -0300

    if, else, with and for

--- 7_TEMPLATE_ENGINES/index.js
+++ 7_TEMPLATE_ENGINES/index.js
@@ -6,8 +6,36 @@ const app = express();
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
 app.get("/", (req, res) => {
-  res.render("home", { layout: false });
+  const user = {
+    name: "thiago",
+    age: 28,
+  };
+
+  const title = "HandleBar Express";
+
+  const auth = true;
+
+  const approved = false;
+
+  const items = ["item a", "item b"];
+
+  res.render("home", { user, title, auth, approved, items });
+});
+
+app.get("/post", (req, res) => {
+  const post = {
+    title: "Aprender nodejs",
+    category: "Node",
+    body: "Curso na udemy",
+    comments: 4,
+  };
+
+  res.render("blog", { post });
 });
 
 app.listen(3000);

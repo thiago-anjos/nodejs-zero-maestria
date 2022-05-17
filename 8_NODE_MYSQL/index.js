a79ef632ff081e90767524525e3fe0f04bfe2e62
thiago-anjos <your_email@example.com>
2022-05-17 15:22:11 -0300

    edit book

--- 8_NODE_MYSQL/index.js
+++ 8_NODE_MYSQL/index.js
@@ -64,6 +64,35 @@ app.post("/books/insertbook", (req, res) => {
   });
 });
 
+app.get("/books/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const book = data[0];
+    res.render("editbook", { book });
+  });
+});
+
+app.post("/books/updatebook", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `UPDATE books SET title= '${title}', pageqty = '${pageqty}' WHERE id = ${id}`;
+
+  conn.query(sql, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/books");
+  });
+});
+
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",

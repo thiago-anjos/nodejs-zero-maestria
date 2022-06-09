2342689bfb7728a50ecdc34df3368df2eebc6441
thiago-anjos <your_email@example.com>
2022-06-09 19:07:26 -0300

    using sequelize find all, async await and foreach to handle data

--- 9_SEQUELIZE/index.js
+++ 9_SEQUELIZE/index.js
@@ -20,8 +20,33 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.post("/users/create", async (req, res) => {
+  let name = req.body.name;
+  let occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  newsletter === "on" ? (newsletter = true) : (newsletter = false);
+
+  console.log("dados de criação do usuário", req.body);
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect("/");
+});
+
+app.get("/users/create", (req, res) => {
+  res.render("addUser");
+});
+
+app.get("/", async (req, res) => {
+  const users = await User.findAll({ raw: true });
+
+  //foreach altera o array original
+  users.forEach(function (a) {
+    a.newsletter = a.newsletter === 1 ? "Sim" : "Não";
+  });
+
+  res.render("home", { users });
 });
 
 //forma de manter a conexão persistente

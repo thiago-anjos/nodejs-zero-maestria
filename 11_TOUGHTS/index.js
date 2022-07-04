f62ad099e3e30af1521ba5a96a406c2255c3d2a4
thiago-anjos <your_email@example.com>
2022-07-04 12:29:32 -0300

    create models User and Tought

---
+++ 11_TOUGHTS/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+//Models
+const Tought = require("./models/Tought");
+const User = require("./models/User");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+//receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//session middleware, diz onde o express vai salvar as sessões
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+//flash messages
+app.use(flash());
+
+//public pach
+app.use(express.static("public"));
+
+//set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+  next();
+});
+
+conn
+  //.sync({force:true})  => serve para recriar as tabelas !!!! cuidado !!!! isso apaga todos os dados
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

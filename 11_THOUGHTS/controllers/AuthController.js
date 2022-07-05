0434f29c232ee643bd1c3972632e9d140ac015bb
thiago-anjos <your_email@example.com>
2022-07-05 12:48:33 -0300

    registerPost async method

--- 11_THOUGHTS/controllers/AuthController.js
+++ 11_THOUGHTS/controllers/AuthController.js
@@ -1,8 +1,22 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
   }
   static register(req, res) {
     res.render("auth/register");
+  }
+  static async registerPost(req, res) {
+    const { username, email, password, confirmpassword } = req.body;
+    //password match validation
+    if (password != confirmpassword) {
+      // send message to front using flash messages
+      req.flash("message", "As senhas não confererem, tente novamente");
+      res.render("auth/register");
+      // finalizar função
+      return;
+    }
   }
 };

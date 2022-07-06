f8bbe0ec960559573cf8afa5b14191cacff49e38
thiago-anjos <your_email@example.com>
2022-07-06 06:36:08 -0300

    authentication

--- 11_THOUGHTS/controllers/AuthController.js
+++ 11_THOUGHTS/controllers/AuthController.js
@@ -5,6 +5,34 @@ module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
   }
+
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+    //find user
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      req.flash("message", "usuário não encontrado");
+      res.render("auth/login");
+      return;
+    }
+
+    //check if passwords match
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+    if (!passwordMatch) {
+      req.flash("message", "senha inválida");
+      res.render("auth/login");
+      return;
+    }
+
+    //initialize session
+    req.session.userid = user.id;
+    req.flash("message", "Autenticação realiada com sucesso");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+
   static register(req, res) {
     res.render("auth/register");
   }
